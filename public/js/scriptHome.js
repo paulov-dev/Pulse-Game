30ab874ab28c6d696a72a21ce09ae68b035d4935
paulov-dev <paulovmmf@gmail.com>
2023-10-01 20:08:33 -0300

    Atualização Geral

--- public/js/scriptHome.js
+++ public/js/scriptHome.js
@@ -1,4 +1,11 @@
 function logar(){
-   
-    window.location.href = '/pages/index.html'
+    window.location.href = '/public/pages/index.html'
+}
+
+function doc(){
+    window.location.href = 'https://docs.pulsegame.com.br'
+}
+
+function home(){
+    window.location.href = '/'
 }
